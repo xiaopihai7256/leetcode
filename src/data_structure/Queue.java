46133d04233d784aac12267f560bcad2024c98a5
xiaopihai7256 <xiaopihai7256@gmail.com>
2020-08-22 15:05:04 +0800

    add: BFS & Queue

---
+++ src/data_structure/Queue.java
@@ -0,0 +1,113 @@
+package data_structure;
+
+import java.util.Iterator;
+
+/**
+ * date: 2020/8/22
+ * description: Queue
+ *
+ * @author huifei.liu@hand-chian.com
+ */
+public class Queue<T> implements Iterable<T> {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>(10);
+        for (int i = 0; i <= 10; i++) {
+            queue.enQueue(i);
+        }
+        System.out.println(queue);
+        for (Integer num : queue) {
+            System.out.println(num);
+        }
+    }
+
+    private final Object[] queue;
+    private int head;
+    private int tail;
+
+    public Queue(int length) {
+        this.queue = new Object[length];
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    public boolean enQueue(T ele) {
+        if (size() == queue.length - 1) {
+            return false;
+        }
+        queue[tail] = ele;
+        if (tail == queue.length - 1) {
+            tail = 0;
+        } else {
+            tail = tail + 1;
+        }
+        return true;
+    }
+
+    public T deQueue() {
+        if (isEmpty()) {
+            return null;
+        }
+        T ele = eleData(head);
+        if (head == queue.length - 1) {
+            head = 0;
+        } else {
+            head = head + 1;
+        }
+        return ele;
+    }
+
+    public int size() {
+        if (head > tail) return queue.length - (head - tail);
+        else return tail - head;
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public String toString() {
+        if (isEmpty()) return "[]";
+        StringBuilder builder = new StringBuilder("[");
+        int count = size();
+        for (int index = head; count > 0; count--) {
+            builder.append(queue[index].toString()).append(',');
+            if (index == queue.length - 1) {
+                index = 0;
+            } else {
+                index = index + 1;
+            }
+        }
+        builder.setCharAt(builder.length() - 1, ']');
+        return builder.toString();
+    }
+
+    private class QueueIterator implements Iterator<T> {
+
+        private int cursor = size();
+        private int index = head;
+
+        @Override
+        public boolean hasNext() {
+            return cursor > 0;
+        }
+
+        @Override
+        public T next() {
+            T result = eleData(index);
+            cursor--;
+            index = index == queue.length - 1 ? 0 : index + 1;
+            return result;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private T eleData(int i) {
+        return (T)queue[i];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new QueueIterator();
+    }
+}
---
+++ src/leetcode/LevelOrder.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * date: 2020/8/19
+ * description: https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+ * @author huifei.liu@hand-chian.com
+ */
+public class LevelOrder {
+
+    /**
+     * 2ms
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<List<Integer>> result = new LinkedList<>();
+        addLevelOrder(result, Collections.singletonList(root));
+        return result;
+    }
+
+    public static void addLevelOrder(List<List<Integer>> result, List<TreeNode> nodes) {
+        if (nodes == null || nodes.size() <= 0) return;
+        result.add(valueList(nodes));
+        List<TreeNode> nextLevel = new LinkedList<>();
+        for (TreeNode node : nodes) {
+            if (node.left != null) nextLevel.add(node.left);
+            if (node.right!= null) nextLevel.add(node.right);
+        };
+        addLevelOrder(result, nextLevel);
+    }
+
+    static List<Integer> valueList(List<TreeNode> nodes) {
+        List<Integer> result = new ArrayList<>(nodes.size());
+        for (TreeNode node : nodes) {
+            result.add(node.val);
+        }
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
